f7beeb3dd6daf714895713ca031437d1639920c7
namekuzisio <92626029+namekuzisio@users.noreply.github.com>
2022-11-26 19:54:58 +0900

    Add files via upload

---
+++ tutClicker/tutClicker/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace tutClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbToggle = new System.Windows.Forms.CheckBox();
+            this.sldCPS = new Siticone.Desktop.UI.WinForms.SiticoneTrackBar();
+            this.lbCPS = new System.Windows.Forms.Label();
+            this.LeftClicker = new System.Windows.Forms.Timer(this.components);
+            this.Random = new System.Windows.Forms.Timer(this.components);
+            this.cbSiftD = new System.Windows.Forms.CheckBox();
+            this.btBind = new System.Windows.Forms.Button();
+            this.Bind = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbToggle
+            // 
+            resources.ApplyResources(this.cbToggle, "cbToggle");
+            this.cbToggle.Name = "cbToggle";
+            this.cbToggle.UseVisualStyleBackColor = true;
+            // 
+            // sldCPS
+            // 
+            resources.ApplyResources(this.sldCPS, "sldCPS");
+            this.sldCPS.Maximum = 20;
+            this.sldCPS.Minimum = 5;
+            this.sldCPS.Name = "sldCPS";
+            this.sldCPS.Style = Siticone.Desktop.UI.WinForms.Enums.TrackBarStyle.Metro;
+            this.sldCPS.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
+            this.sldCPS.Value = 10;
+            this.sldCPS.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sldCPS_Scroll);
+            // 
+            // lbCPS
+            // 
+            resources.ApplyResources(this.lbCPS, "lbCPS");
+            this.lbCPS.Name = "lbCPS";
+            // 
+            // LeftClicker
+            // 
+            this.LeftClicker.Enabled = true;
+            this.LeftClicker.Tick += new System.EventHandler(this.LeftClicker_Tick);
+            // 
+            // Random
+            // 
+            this.Random.Enabled = true;
+            this.Random.Tick += new System.EventHandler(this.Random_Tick);
+            // 
+            // cbSiftD
+            // 
+            resources.ApplyResources(this.cbSiftD, "cbSiftD");
+            this.cbSiftD.Name = "cbSiftD";
+            this.cbSiftD.UseVisualStyleBackColor = true;
+            // 
+            // btBind
+            // 
+            resources.ApplyResources(this.btBind, "btBind");
+            this.btBind.Name = "btBind";
+            this.btBind.UseVisualStyleBackColor = true;
+            this.btBind.Click += new System.EventHandler(this.btBind_Click);
+            this.btBind.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btBind_KeyUp);
+            // 
+            // Bind
+            // 
+            this.Bind.Enabled = true;
+            this.Bind.Interval = 150;
+            this.Bind.Tick += new System.EventHandler(this.Bind_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btBind);
+            this.Controls.Add(this.cbSiftD);
+            this.Controls.Add(this.lbCPS);
+            this.Controls.Add(this.sldCPS);
+            this.Controls.Add(this.cbToggle);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbToggle;
+        private Siticone.Desktop.UI.WinForms.SiticoneTrackBar sldCPS;
+        private System.Windows.Forms.Label lbCPS;
+        private System.Windows.Forms.Timer LeftClicker;
+        private System.Windows.Forms.Timer Random;
+        private System.Windows.Forms.CheckBox cbSiftD;
+        private System.Windows.Forms.Button btBind;
+        private System.Windows.Forms.Timer Bind;
+    }
+}
+
---
+++ tutClicker/tutClicker/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tutClicker
+{
+    public partial class Form1 : Form
+    {
+        #region dlls
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey); // Keys enumeration
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sldCPS_Scroll(object sender, ScrollEventArgs e)
+        {
+            lbCPS.Text = $"{sldCPS.Value}";
+        }
+
+
+        bool isSifting = false;
+        private void LeftClicker_Tick(object sender, EventArgs e)
+        {
+            LeftClicker.Interval = 1000 / randomisedCPS;
+
+            Process[] processes = Process.GetProcessesByName("javaw");
+            foreach (Process process in processes)
+            {
+                if (GetForegroundWindow() == FindWindow(null, process.MainWindowTitle))
+                {
+                    if(cbToggle.Checked && !isSifting)
+                        if (MouseButtons == MouseButtons.Left)
+                        {
+                            SendMessage(GetForegroundWindow(), 0x201, 0, 0);
+                            Task.Delay(20).Wait();
+                            SendMessage(GetForegroundWindow(), 0x202, 0, 0);
+                        }
+;
+                }
+            }
+
+            isSifting = (GetAsyncKeyState(Keys.LShiftKey) < 0);
+
+        }
+
+        int randomisedCPS = 10;
+        private void Random_Tick(object sender, EventArgs e)
+        {
+            randomisedCPS = new Random().Next(sldCPS.Value - 3, sldCPS.Value + 3);
+        }
+
+        private void btBind_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (btBind.Text == "...")
+            {
+                if (GetAsyncKeyState(Keys.Escape) < 0)
+                {
+                    btBind.Text = "none";
+                }
+                else
+                {
+                    btBind.Text = e.KeyCode.ToString();
+                }
+            }
+        }
+
+        private void btBind_Click(object sender, EventArgs e)
+        {
+            btBind.Text = "...";
+        }
+
+        public static KeysConverter Key = new KeysConverter();
+
+
+        private void Bind_Tick(object sender, EventArgs e)
+        {
+            if (btBind.Text != "none")
+            {
+                if (btBind.Text != "...")
+                {
+                    Keys key = (Keys)Key.ConvertFromString(btBind.Text);
+
+                    if (GetAsyncKeyState(key) < 0)
+                    {
+                        cbToggle.Checked = !cbToggle.Checked;
+                    }
+                }
+            }
+        }
+    }
+}
